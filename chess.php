4066be0331de4d229c0aa1ced2853d25614051bf
Eyvette Thompson <eyvettet@gmail.com>
2017-10-15 16:54:01 -0400

    Add files via upload

--- chess.php
+++ chess.php
@@ -13,6 +13,10 @@
                 
             }
             
+            .white{
+               background-color:white; 
+                
+            }
                
         </style>
     </head>
@@ -24,7 +28,9 @@ $cols = $_REQUEST['cols'];
 
 print('<table style="width:200px; height:200px; background-color:white; border:2px solid black;">');
 
-
+if(($rows > 1) && ($rows < 51) && ($cols > 1) && ($cols < 51))
+{
+        
 $y=0;
     while($y < $rows)
         {       
@@ -49,6 +55,35 @@ $y=0;
     
         }
 print('</table>');
+}
+else
+{
+    $y=0;
+    while($y < 8)
+        {       
+        print('<tr>');
+        $z = 0;
+        while($z < 8)
+        {  if(($y + $z) % 2 == 0)
+            { 
+                print('<td class="black"; style="width:30px; height:30px; "></td>');
+            }
+            else
+            {
+                print('<td class="white"; style="width:30px; height:30px; "></td>');
+            }
+                $z++;
+        
+        }
+    
+        $y++;
+
+        print('</tr>');   
+    
+        }
+print('</table>');
+}
+
 
 ?>
         
